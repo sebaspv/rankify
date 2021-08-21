0ac0fab092c70202b36f027af6fe753d45936e0b
Khalid Filali <56809036+KhalidFilali@users.noreply.github.com>
2021-08-21 16:54:25 -0400

    styling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import home from './home.svg'
 import './App.css';
 import Form from './form.js'
 
@@ -12,6 +13,7 @@ function App() {
       <p class="navo">How It Works</p>
       </div>
       <div className="fade-in">
+      <img src={home} className="bg"  alt="logo" />
       <h1>Get Your Logo Rated in seconds, by AI.</h1>
       <p class="fade-in" id="text">Simply upload your logo using the button below and our machine learning algorithms will analyze your logo, score it based off of fundamental design principles (color scheme, design, shape, e.t.c) and even guess what industry you're in, in seconds.</p>
       </div>
