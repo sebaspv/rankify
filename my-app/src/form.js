e86086af9fdb37e59d36d9f6489918805c9206e5
Khalid Filali <56809036+KhalidFilali@users.noreply.github.com>
2021-08-21 17:51:37 -0400

    Update form.js

--- my-app/src/form.js
+++ my-app/src/form.js
@@ -4,13 +4,14 @@ import './App.css'
 function Form(){
   function handleSubmit(e) {
     e.preventDefault();
+    console.log(e.target.value)
   }
     return(
     <div class="container">
       <div class="button-wrap">
         <form>
-        <input type="text" placeholder="Put the link to your image here!" id="upload"/>
-        <input type="submit" value="Upload File" class="button" onClick={handleSubmit}/>
+        <input type="text" placeholder="Put the link to your image here!" id="upload" onSubmit={handleSubmit}/>
+        <input type="submit" value="Upload File" class="button"/>
         </form>
       </div>
     </div>
