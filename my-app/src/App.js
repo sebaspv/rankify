7a153baed667d87365bcc35e0b0eacdbf7285fb9
Khalid Filali <56809036+KhalidFilali@users.noreply.github.com>
2021-08-21 12:51:16 -0400

    more styling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,14 +6,13 @@ import Form from './form.js'
 function App() {
   return (
     <div className="App">
-      <header class="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <div class="fade-in">
-      <h1 id="heading">Get Your Logo Rated in seconds, by AI.</h1>
-      <p id="text">Simply upload your image using the button below and our machine learning algorithms will analyze your logo, score it based off of fundamental design principles (color scheme, design, shape, e.t.c) and even guess what industry you're in, in seconds.</p>
+      <div class="header">
+      <img src={logo} className="App-logo" alt="logo" />
       </div>
-
+      <div className="fade-in">
+      <h1>Get Your Logo Rated in seconds, by AI.</h1>
+      <p class="fade-in" id="text">Simply upload your logo using the button below and our machine learning algorithms will analyze your logo, score it based off of fundamental design principles (color scheme, design, shape, e.t.c) and even guess what industry you're in, in seconds.</p>
+      </div>
       <Form></Form>
     </div>
 
